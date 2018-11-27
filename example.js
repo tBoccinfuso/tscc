972e574749bc4c78195a9b698f2cdf4767622702
Thomas Boccinfuso <tboccinfuso@gmail.com>
2018-11-26 22:50:36 -0500

    Delete example.js

--- example.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Example = /** @class */ (function () {
-    function Example() {
-    }
-    Example.prototype.run = function () {
-        alert('hello from Typescript');
-    };
-    return Example;
-}());
